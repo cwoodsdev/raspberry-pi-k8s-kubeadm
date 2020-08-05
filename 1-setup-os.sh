976880907c9910e62ab72cca19a8bffbecee49b8
Chris Woods <65170272+cwoodsdev@users.noreply.github.com>
2020-08-05 19:34:00 +0100

    Update 1-setup-os.sh

--- 1-setup-os.sh
+++ 1-setup-os.sh
@@ -2,8 +2,30 @@
 
 sudo apt-get update
 
+#Docker setup
 sudo apt-get install -y docker.io
 
 sudo systemctl start docker
 sudo systemctl enable docker
 
+sudo docker info
+
+sudo cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/firmware/cmdline.txt
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
