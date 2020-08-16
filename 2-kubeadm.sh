8d929c4003b0026f82619e8b3f4e48083e4172a9
Chris Woods <65170272+cwoodsdev@users.noreply.github.com>
2020-08-16 17:17:01 +0100

    Update 2-kubeadm.sh

--- 2-kubeadm.sh
+++ 2-kubeadm.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+sudo snap install kubelet --classic
+systemctl enable kubelet.service
 
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+sudo snap install kubeadm --classic
+
+# Run on all nodes control and workwer
+sudo snap install kubectl --classic
+
 
